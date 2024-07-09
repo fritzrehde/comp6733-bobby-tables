6dfbeff7c2ccc2282d64a6305c459d78f90eb9d8
Harry's Laptop <wilson2763@gmail.com>
2024-07-10 09:47:18 +1000

    init commit

---
+++ data-collection/knn.py
@@ -0,0 +1,17 @@
+# VERSION Python 3.11.4
+
+
+def knn(RSSI_train, LOCATION_train, RSSI_test, k):
+    """
+    Perform k-nearest neighbors classification.
+
+    Parameters:
+    RSSI_train (array-like): Training data features.
+    LOCATION_train (array-like): Training data labels.
+    RSSI_test (array-like): Test data features.
+    k (int): Number of neighbors to consider.
+
+    Returns:
+    array-like: Predicted labels for the test data.
+    """
+    pass
