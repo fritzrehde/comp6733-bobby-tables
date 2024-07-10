9e808632aae14bc75e6646f23cea47376e469d27
Harry's Laptop <wilson2763@gmail.com>
2024-07-10 11:01:42 +1000

    begin implementation of sklearn knn

--- data-processing/knn.py
+++ data-processing/knn.py
@@ -3,13 +3,12 @@
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
 
-def knn(RSSI_train, LOCATION_train, RSSI_test, k):
+def knn(RSSI_train, RSSI_test, k): # location indices are implicit
     """
     Perform k-nearest neighbors classification.
 
     Parameters:
     RSSI_train (array-like): Training data features.
-    LOCATION_train (array-like): Training data labels.
     RSSI_test (array-like): Test data features.
     k (int): Number of neighbors to consider.
 
@@ -23,6 +22,7 @@ def knn(RSSI_train, LOCATION_train, RSSI_test, k):
         # ...
     ]
 
+    # LOCATION IS IMPLICIT
     LOCATION_train = [
         1,  # Location label for sample 1
         2,  # Location label for sample 2
@@ -42,6 +42,14 @@ def knn(RSSI_train, LOCATION_train, RSSI_test, k):
         # ...
     ]
     """
-    X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-    nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
-    distances, indices = nbrs.kneighbors(X)
+    nbrs = NearestNeighbors(n_neighbors=k, algorithm='ball_tree').fit(RSSI_train)
+    distances, indices = nbrs.kneighbors(RSSI_test)
+    print("DIST:")
+    print(distances)
+    print()
+    print("INDEX: ")
+    print(indices)
+if __name__ == "__main__":
+    RSSI_train = np.array([[-60, -65, -70], [-55, -60, -75], [-50, -55, -80]])
+    RSSI_test = np.array([[-58, -63, -68], [-53, -58, -73]])
+    knn(RSSI_train, RSSI_test, 2)
