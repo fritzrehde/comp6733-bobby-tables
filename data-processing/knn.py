4cd6a9eac18060b3ca375544aa661d41318f7112
Harry's Laptop <wilson2763@gmail.com>
2024-07-10 10:28:05 +1000

    file restructuring

--- data-processing/knn.py
+++ data-processing/knn.py
@@ -1,5 +1,7 @@
 # VERSION Python 3.11.4
 
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
 
 def knn(RSSI_train, LOCATION_train, RSSI_test, k):
     """
@@ -40,4 +42,9 @@ def knn(RSSI_train, LOCATION_train, RSSI_test, k):
         # ...
     ]
     """
-    pass
+    
+    X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+    nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
+    distances, indices = nbrs.kneighbors(X)
+    indices
+    distances
